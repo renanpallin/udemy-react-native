047648ac7392bbde321ef5796c39436fd3fd3d42
Renan Pallin <renanpallin@gmai.com>
2018-01-31 03:00:09 -0200

    Renderizando nossa primeira lista

--- people/App.js
+++ people/App.js
@@ -4,11 +4,34 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Header title="Pessoas!" />
-      </View>
-    );
-  }
+	renderList() {
+		const names = [
+			'Eddie Van Halen',
+			'Jimi Hendrix',
+			'Chimbinha',
+			'Steve Vai',
+		];
+
+		/*
+		[
+			<Text>Eddie Van Halen</Text>,
+			<Text>Jimi Hendrix</Text>,
+			...
+		]
+		 */
+		const textElements = names.map(name => {
+			return <Text>{name}</Text>;
+		});
+
+		return textElements;
+	}
+
+	render() {
+		return (
+			<View>
+				<Header title="Pessoas!" />
+				{this.renderList()}
+			</View>
+		);
+	}
 }

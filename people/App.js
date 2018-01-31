753bd8002aa53c83ebc645d5f4d731907e40f62b
Renan Pallin <renanpallin@gmai.com>
2018-01-31 01:40:40 -0200

    [People] Criando o header

---
+++ people/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
---
+++ people/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = (props) => (
+	<View>
+		<Text>Este é o nosso Header!</Text>
+	</View>
+);
+
+export default Header;

784eb236be958586550469b96a96af06ce0a6ab4
Renan Pallin <renanpallin@gmai.com>
2018-02-01 00:22:41 -0200

    Movendo a chamada para componentDidMount

--- people/App.js
+++ people/App.js
@@ -6,19 +6,32 @@ import Header from './src/components/Header';
 import axios from 'axios';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			peoples: []
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://randomuser.me/api/?nat=br&results=5')
+			.then(response => {
+				const { results } = response.data;
+				this.setState({
+					peoples: results
+				});
+			})
+	}
+
 	renderList() {
 		// const textElements = names.map(name => {
 		// 	return <Text key={name}>{name}</Text>;
 		// });
 
 		/* Promises */
-		axios
-			.get('https://randomuser.me/api/?nat=br&results=5')
-			.then(response => {
-				const { results } = response.data;
-				const names = results.map(people => people.name.first);
-				console.log(names);
-			})
+
 
 		// return textElements;
 	}
@@ -27,7 +40,6 @@ export default class App extends React.Component {
 		return (
 			<View>
 				<Header title="Pessoas!" />
-				{this.renderList()}
 			</View>
 		);
 	}

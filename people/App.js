23e8a0322c3262dd9e176724aa13f40d7f51e980
Renan Pallin <renanpallin@gmai.com>
2018-02-18 03:09:11 -0300

    Estrutura inicial do StackNavigator

--- people/App.js
+++ people/App.js
@@ -1,3 +1,12 @@
+import { StackNavigator } from 'react-navigation';
+
 import PeoplePage from './src/pages/PeoplePage';
 
-export default PeoplePage;
+
+export default StackNavigator({
+	'Main': {
+		screen: PeoplePage
+	}
+});
+
+// export default PeoplePage;

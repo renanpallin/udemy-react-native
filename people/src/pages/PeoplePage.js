4081236f08869a7d37c7d9f3d0e054f6a4b7c2c9
Renan Pallin <renanpallin@gmai.com>
2018-02-22 02:47:24 -0300

    Renderizando condicionalmente um componente

--- people/src/pages/PeoplePage.js
+++ people/src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 
 import PeopleList from '../components/PeopleList';
@@ -9,24 +9,41 @@ export default class PeoplePage extends React.Component {
 		super(props);
 
 		this.state = {
-			peoples: []
+			peoples: [],
+			loading: false,
 		};
 	}
 
 	componentDidMount() {
-		axios
-			.get('https://randomuser.me/api/?nat=br&results=15')
-			.then(response => {
-				const { results } = response.data;
-				this.setState({
-					peoples: results
-				});
-			})
+		this.setState({ loading: true });
+		setTimeout(() => {
+			axios
+				.get('https://randomuser.me/api/?nat=br&results=15')
+				.then(response => {
+					const { results } = response.data;
+					this.setState({
+						peoples: results,
+						loading: false,
+					});
+				})
+		}, 1500)
 	}
+
+	// renderLoading() {
+	// 	if (this.state.loading)
+	// 		return <ActivityIndicator size="large" color="#6ca2f7" />;
+	// 	return null;
+	// }
 
 	render() {
 		return (
 			<View>
+				{/* this.renderLoading() */}
+				{
+					this.state.loading
+						? <ActivityIndicator size="large" color="#6ca2f7" />
+						: null
+				}
 				<PeopleList
 					peoples={this.state.peoples}
 					onPressItem={pageParams => {

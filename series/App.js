0d65b391a8a6280806ff2e376dc865fb6d7174a1
Renan Pallin <renanpallin@gmai.com>
2018-03-10 01:38:30 -0300

    Criando a LoginPage e configurando navegation

--- series/App.js
+++ series/App.js
@@ -1,23 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+import LoginPage from './src/pages/LoginPage';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default StackNavigator({
+    'Login': {
+        screen: LoginPage
+    }
+})
---
+++ series/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class LoginPage extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Login!!!</Text>
+			</View>
+		)
+	}
+}

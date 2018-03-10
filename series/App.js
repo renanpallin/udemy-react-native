860c95bafe3e3119fb5b0a47266314223528a20c
Renan Pallin <renanpallin@gmai.com>
2018-03-10 01:55:27 -0300

    Configurações iniciais para nosso Header

--- series/App.js
+++ series/App.js
@@ -4,6 +4,23 @@ import LoginPage from './src/pages/LoginPage';
 
 export default StackNavigator({
     'Login': {
-        screen: LoginPage
+        screen: LoginPage,
+        navigationOptions: {
+            title: 'Bem vindo!',
+        }
     }
-})
+}, {
+    navigationOptions: {
+        title: "Series!",
+        headerTintColor: 'white',
+        headerStyle: {
+            backgroundColor: '#6ca2f7',
+            borderBottomWidth: 1,
+            borderBottomColor: '#C5C5C5',
+        },
+        headerTitleStyle: {
+            color: 'white',
+            fontSize: 30,
+        }
+    }
+});

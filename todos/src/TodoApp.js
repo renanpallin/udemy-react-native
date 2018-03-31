c11d41dc4a70d5db398204af2cd641978f02c0c6
Renan Pallin <renanpallin@gmai.com>
2018-03-31 07:06:27 -0300

    Movendo nosso componente root pra TodoApp

---
+++ todos/src/TodoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TodoApp extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Olá!</Text>
+			</View>
+		);
+	}
+}

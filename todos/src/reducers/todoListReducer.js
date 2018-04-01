b8349c7b255b93ef3591d0052c50f880174fbe3d
Renan Pallin <renanpallin@gmai.com>
2018-04-01 05:07:12 -0300

    Colocando ids nos nossos todos

--- todos/src/reducers/todoListReducer.js
+++ todos/src/reducers/todoListReducer.js
@@ -1,10 +1,13 @@
 import { ADD_TODO } from '../actions';
 
+let nextId = 1;
+
 const todoListReducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD_TODO:
 			console.log('addTodo foi chamado!!', action.text)
 			const newTodo = {
+				id: nextId++,
 				text: action.text
 			}
 			return [...state, newTodo]

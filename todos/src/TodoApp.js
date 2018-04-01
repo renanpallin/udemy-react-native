5568fbdf48199c95f6282682f29bcc9ef8d96e87
Renan Pallin <renanpallin@gmai.com>
2018-04-01 01:34:11 -0300

    Envolvendo nosso app com Provider e instalando redux e react-redux

--- todos/src/TodoApp.js
+++ todos/src/TodoApp.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import TodoForm from './components/TodoForm';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 export default class TodoApp extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<TodoForm />
-			</View>
+			<Provider store={store}>
+				<View style={styles.container}>
+					<TodoForm />
+				</View>
+			</Provider>
 		);
 	}
 }

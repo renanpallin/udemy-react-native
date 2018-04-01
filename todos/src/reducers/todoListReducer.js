4d3c2453e4229094166a205b33f874526f7818fd
Renan Pallin <renanpallin@gmai.com>
2018-04-01 06:02:38 -0300

    todoListReducer respondendo por TOGGLE_TODO

--- todos/src/reducers/todoListReducer.js
+++ todos/src/reducers/todoListReducer.js
@@ -1,16 +1,25 @@
-import { ADD_TODO } from '../actions';
+import { ADD_TODO, TOGGLE_TODO } from '../actions';
 
 let nextId = 1;
 
 const todoListReducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD_TODO:
-			console.log('addTodo foi chamado!!', action.text)
 			const newTodo = {
 				id: nextId++,
-				text: action.text
+				text: action.text,
+				done: false
 			}
 			return [...state, newTodo]
+		case TOGGLE_TODO:
+			return state.map(todo => {
+				if (todo.id === action.todoId)
+					return {
+						...todo,
+						done: !todo.done
+					};
+				return todo;
+			});
 		default:
 			return state;
 	}

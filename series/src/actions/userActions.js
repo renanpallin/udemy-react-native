b5b6caeae513bec04bccdab1856cdb5c63b12f20
Renan Pallin <renanpallin@gmai.com>
2018-05-04 11:35:33 -0300

    SeriesApp - Finalizando a LoginPage

--- series/src/actions/userActions.js
+++ series/src/actions/userActions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Alert } from 'react-native';
 
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
 const userLoginSuccess = user => ({
@@ -18,31 +19,32 @@ export const tryLogin = ({ email, password }) => dispatch => {
 		.then(user => {
 			const action = userLoginSuccess(user);
 			dispatch(action);
+			return user;
 		})
-		// .catch(error => {
-		// 	if (error.code === 'auth/user-not-found') {
-		// 		Alert.alert(
-		// 			'Usuário não encontrado',
-		// 			'Deseja criar um cadastro com as informações inseridas?',
-		// 			[{
-		// 				text: 'Não',
-		// 				onPress: () => console.log('Usuário não quer criar conta'),
-		// 				style: 'cancel' // IOS
-		// 			}, {
-		// 				text: 'Sim',
-		// 				onPress: () => {
-		// 					firebase
-		// 						.auth()
-		// 						.createUserWithEmailAndPassword(mail, password)
-		// 						.then(loginUserSuccess)
-		// 						.catch(loginUserFailed)
-		// 				}
-		// 			}],
-		// 			{ cancelable: false }
-		// 		)
-		// 		return;
-		// 	}
-		// 	loginUserFailed(error);
-		// })
-		// .then(() => this.setState({ isLoading: false }));
+		.catch(error => {
+			if (error.code === 'auth/user-not-found') {
+				return new Promise((resolve, reject) => {
+					Alert.alert(
+						'Usuário não encontrado',
+						'Deseja criar um cadastro com as informações inseridas?',
+						[{
+							text: 'Não',
+							onPress: () => resolve(),
+							style: 'cancel' // IOS
+						}, {
+							text: 'Sim',
+							onPress: () => {
+								firebase
+									.auth()
+									.createUserWithEmailAndPassword(email, password)
+									.then(resolve)
+									.catch(reject)
+							}
+						}],
+						{ cancelable: false }
+					)
+				})
+			}
+			return Promise.reject(error)
+		})
 }
--- series/src/pages/LoginPage.js
+++ series/src/pages/LoginPage.js
@@ -51,9 +51,20 @@ class LoginPage extends React.Component {
 		const { mail: email, password } = this.state;
 
 		this.props.tryLogin({ email, password })
-			.then(() => {
-				this.setState({ message: "Sucesso!" });
-				this.props.navigation.replace('Main');
+			.then(user => {
+				if (user)
+					return this.props.navigation.replace('Main');
+
+				this.setState({
+					isLoading: false,
+					message: ''
+				});
+			})
+			.catch(error => {
+				this.setState({
+					isLoading: false,
+					message: this.getMessageByErrorCode(error.code)
+				});
 			});
 	}
 

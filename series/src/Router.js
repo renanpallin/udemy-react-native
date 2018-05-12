0d22f6c0b241416168b37c5ed0f5bc5e6411144d
Renan Pallin <renanpallin@gmai.com>
2018-05-12 12:04:22 -0300

    Desgambiarrando nosso app

--- series/src/Router.js
+++ series/src/Router.js
@@ -5,27 +5,17 @@ import SeriesPage from './pages/SeriesPage';
 import SerieDetailPage from './pages/SerieDetailPage';
 
 export default StackNavigator({
+    'Main': {
+        screen: SeriesPage
+    },
     'SerieDetail': {
         screen: SerieDetailPage,
         navigationOptions: ({ navigation }) => {
-            // const { serie } = navigation.state.params;
-            const serie = {
-                "id": 3,
-                "title": "Todo mundo odeia o Chris",
-                "gender": "Comédia",
-                "rate": 100,
-                "img":
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NTg4MTc5OF5BMl5BanBnXkFtZTcwMzc2MzYzMQ@@._V1_.jpg",
-                "description":
-                    "Everybody Hates Chris (Todo Mundo Odeia o Chris (título no Brasil) ou Todos Contra o Chris (título em Portugal)) é uma série de televisão estadunidense de comédia dramática inspirado nas experiências pessoais de Chris Rock no bairro de Bed-Stuy, em Nova Iorque, seguindo um estilo muito parecido com a série Anos Incríveis."
-            }
+            const { serie } = navigation.state.params;
             return {
                 title: serie.title
             }
         }
-    },
-    'Main': {
-        screen: SeriesPage
     },
     'Login': {
         screen: LoginPage,
--- series/src/pages/SerieDetailPage.js
+++ series/src/pages/SerieDetailPage.js
@@ -6,17 +6,7 @@ import LongText from '../components/LongText';
 
 class SerieDetailPage extends React.Component {
 	render() {
-		const serie = {
-			"id": 3,
-			"title": "Todo mundo odeia o Chris",
-			"gender": "Comédia",
-			"rate": 100,
-			"img":
-				"https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2NTg4MTc5OF5BMl5BanBnXkFtZTcwMzc2MzYzMQ@@._V1_.jpg",
-			"description":
-				"Everybody Hates Chris (Todo Mundo Odeia o Chris (título no Brasil) ou Todos Contra o Chris (título em Portugal)) é uma série de televisão estadunidense de comédia dramática inspirado nas experiências pessoais de Chris Rock no bairro de Bed-Stuy, em Nova Iorque, seguindo um estilo muito parecido com a série Anos Incríveis."
-		}
-		// this.props.navigation.state.params
+		const { serie } = this.props.navigation.state.params;
 
 		return (
 			<ScrollView>

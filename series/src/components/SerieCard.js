d2cd0c761eab8593cbbcd2ec088bf749b5ef066b
Renan Pallin <renanpallin@gmai.com>
2018-07-15 05:22:47 -0300

    Renderizando imagens apenas quando existem

--- series/src/components/SerieCard.js
+++ series/src/components/SerieCard.js
@@ -17,13 +17,17 @@ const SerieCard = ({ serie, isFirstColumn, onPress }) => (
 			isFirstColumn ? styles.firstColumn : styles.lastColumn
 		]}>
 		<View style={styles.card}>
-			<Image
-				source={{
-					uri: serie.img
-				}}
-				aspectRatio={1}
-				resizeMode="cover"
-			/>
+			{
+				serie.img
+					? <Image
+						source={{
+							uri: serie.img
+						}}
+						aspectRatio={1}
+						resizeMode="cover"
+					/>
+					: null
+			}
 			<View style={styles.cardTitleWrapper}>
 				<Text style={styles.cardTitle}>{serie.title}</Text>
 			</View>
--- series/src/pages/SerieDetailPage.js
+++ series/src/pages/SerieDetailPage.js
@@ -10,11 +10,15 @@ class SerieDetailPage extends React.Component {
 
 		return (
 			<ScrollView>
-				<Image
-					style={styles.image}
-					source={{
-						uri: serie.img
-					}} />
+				{
+					serie.img
+						?  <Image
+							style={styles.image}
+							source={{
+								uri: serie.img
+							}} />
+						: null
+				}
 				<Line label="Título" content={serie.title} />
 				<Line label="Gênero" content={serie.gender} />
 				<Line label="Nota" content={serie.rate} />
